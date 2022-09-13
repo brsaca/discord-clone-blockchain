4620efcb0d22fb2d6a9a674b973414f67885dabc
Brenda SC <brenda.saavedra@fondeadora.com>
2022-09-13 12:59:42 -0500

    chore: add setup webpack

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['encrypted-tbn0.gstatic.com', 'cdn.sanity.io'],
+  },
+  webpack: function (config, options) {
+    config.module.noParse = /gun\.js$/;
+    return config;
+  },
 }
 
 module.exports = nextConfig
