f2148bca0ada8a4a50f6adf5dc1ff75256502099
Brenda SC <brenda.saavedra@fondeadora.com>
2022-09-12 21:10:38 -0500

    feat: add DiscordProvider and useRouter

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { DiscordProvider } from '../context/context'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <DiscordProvider>
+    <Component {...pageProps} />
+  </DiscordProvider>
 }
 
 export default MyApp
